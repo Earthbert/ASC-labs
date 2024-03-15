d37b4077d009480a2faf1e5eabd471896632f6dc
Earthbert <daraban_timotei@tutanota.com>
2024-03-15 12:24:01 +0200

    Add lab3
    Signed-off-by: Earthbert <daraban_timotei@tutanota.com>

---
+++ lab3/task1.py
@@ -0,0 +1,81 @@
+from threading import enumerate, Event, Thread, Condition
+
+class Master(Thread):
+    def __init__(self, max_work : int, work_signal : Condition) -> None:
+        Thread.__init__(self, name = "Master")
+        self.max_work = max_work
+        self.work_signal = work_signal
+    
+    def set_worker(self, worker : 'Worker'):
+        self.worker = worker
+    
+    def run(self):
+        for i in range(self.max_work):
+            # generate work
+            self.work = i  
+            
+            with self.work_signal:
+            # notify worker
+                self.work_signal.notify()
+                self.work_signal.release()
+                
+                # get result
+                self.work_signal.acquire()
+                self.work_signal.wait()
+                if self.get_work() + 1 != self.worker.get_result():
+                    print ("oops")
+                print ("%d -> %d" % (self.work, self.worker.get_result()))
+    
+    def get_work(self):
+        return self.work
+
+class Worker(Thread):
+    def __init__(self, terminate : Event, work_signal : Condition) -> None:
+        Thread.__init__(self, name = "Worker")
+        self.terminate = terminate
+        self.work_signal = work_signal
+
+    def set_master(self, master : Master):
+        self.master = master
+    
+    def run(self):
+        while(True):
+            # wait work
+            with self.work_signal:
+                self.work_signal.wait()
+
+                if(terminate.is_set()): break
+                # generate result
+                self.result = self.master.get_work() + 1
+                # notify master
+                self.work_signal.notify()
+
+    
+    def get_result(self):
+        return self.result
+
+if __name__ ==  "__main__":
+    # create shared objects
+    terminate = Event()
+    work_signal = Condition()
+    
+    # start worker and master
+    w = Worker(terminate, work_signal)
+    m = Master(1000, work_signal)
+    w.set_master(m)
+    m.set_worker(w)
+    w.start()
+    m.start()
+
+    # wait for master
+    m.join()
+
+    # wait for worker
+    with work_signal:
+        work_signal.notify()
+    terminate.set()
+    w.join()
+
+    # print running threads for verification
+    print(enumerate())
+
---
+++ lab3/task3.py
@@ -0,0 +1,45 @@
+"""
+Computer Systems Architecture - Lab 3
+
+Threadpool assignment:
+    Use a pool of threads to search for a DNA sequence in a list of DNA samples
+"""
+from concurrent.futures import ThreadPoolExecutor as ThreadPool
+import random
+
+random.seed(0)
+
+print("DNA search")
+
+NR_DNA_SAMPLES = 1000
+DNA_SAMPLE_LENGTH = 10000
+SEARCH_SEQUENCE_LENGTH = 10
+
+MAX_WORKERS = 30
+
+dna_samples = ["".join([random.choice("ACGT") for _ in range(DNA_SAMPLE_LENGTH)]) for _ in range(NR_DNA_SAMPLES)]
+
+SEARCH_SEQUENCE  = "".join([random.choice("ACGT") for _ in range(SEARCH_SEQUENCE_LENGTH)])
+
+def search_dna_sequence(sequence : str, sample : str) -> bool:
+    return sequence in sample
+
+
+def thread_job(sample_index : int) -> tuple[bool, str]:
+    if search_dna_sequence(SEARCH_SEQUENCE, dna_samples[sample_index]):
+        return True, f"DNA sequence found in sample {sample_index}"
+    return False, f"DNA sequence not found in sample {sample_index}"
+
+
+if __name__ == "__main__":
+
+    thread_pool = ThreadPool(max_workers=MAX_WORKERS)
+
+    futures = []
+
+    with thread_pool:
+        for i in range(NR_DNA_SAMPLES):
+            futures.append(thread_pool.submit(thread_job, i))
+
+    result = [future.result() for future in futures if future.result()[0] == True]
+    print(result[0][1] if result else "DNA sequence not found in any sample")
